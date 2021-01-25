88508d8461711e0db6c7d79a99fdafc0456f7e22
Silverio91 <77552410+Silverio91@users.noreply.github.com>
2021-01-25 12:03:29 -0600

    Create conexion.php

---
+++ RegistroFormulario/bd/conexion.php
@@ -0,0 +1,26 @@
+<?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = ''; #No hay password por ser academico.
+$dbdata = 'forpelis';
+
+
+$conexion = mysqli_connect ($dbhost, $dbuser, $dbpass, $dbdata);
+
+//$conexion  = mysqli_connect ("localhost", "root","","forpelis");
+/*
+if (!$conexion){
+    echo 'Error al conectar a la base de datos';
+}
+else {
+    echo 'Conectado a la base de datos';
+
+}*/
+
+$conexion = new mysqli ($dbhost, $dbuser, $dbpass, $dbdata);
+if ($conexion->connect_error){
+    die('Error de Conexión ... Cancelado proceso');
+}   
+
+?>
+ 
