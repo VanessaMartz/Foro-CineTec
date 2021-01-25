543ab1a671388f3824f943d4d075bec284f68b48
Silverio91 <77552410+Silverio91@users.noreply.github.com>
2021-01-25 12:03:49 -0600

    Create menu.php

---
+++ RegistroFormulario/menu.php
@@ -0,0 +1,42 @@
+<nav class="navbar index-nav fixed-top">
+      <div class="container">
+      <a class="nav-link" href="../nosotros/">Nosotros</a>
+      <li class="dropdown">
+        <a class="dropdown-toggle" href="#" data-toggle="dropdown">Categorías</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="">Acción</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="">Animación</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="">Comedia</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="">Ciencia Ficción</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="">Crimen</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="">Drama</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="">Misterio</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="">Romance</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="">Suspenso</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="">Terror</a>
+        </div>
+      </li>
+      <a id="home" href="../index.php">
+        <img src="../img/ticket-inicio.png">
+      </a>
+      <a class="nav-link" href=""></a>
+      <li class="dropdown">
+        <a class="dropdown-toggle" href="#" data-toggle="dropdown">Iniciar sesión</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="usuario-login/">Usuario</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="adm-login/">Administrador</a>
+        </div>
+      </li>
+      
+  </div>
+  </nav>
