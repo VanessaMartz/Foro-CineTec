3025a69496e90b869090359f0f99180eba38e0d3
Silverio91 <77552410+Silverio91@users.noreply.github.com>
2021-01-25 12:03:55 -0600

    Update recibirdatos.php

--- RegistroFormulario/recibirdatos.php
+++ RegistroFormulario/recibirdatos.php
@@ -1,20 +1,44 @@
 <?php
-    #Servidor       #Cliente
-    $nombre_usuario     = $_GET ['nombre_usuario'];
-    $email_usuario      = $_GET ['email_usuario'];
-    $password_usuario  = $_GET ['password_usuario'];
-    $rol_usuario        = $_GET ['rol_usuario'];
-    
-    #Validacion
-    #Si Grabar BD
-    #Regresar Formulario 
+   
+//include 'bd/conexion.php';
 
-    echo "Los datos recibidos son: <br> 
-    Nombre : $nombre_usuario<br> 
-    Email : $email_usuario <br> 
-    Password : $password_usuario <br> 
-    Rol : $rol_usuario <br>";
-    
-    
+//Recibir los datos y almacenarlos en variables
 
-?>
+$nombre_usuario     = $_POST["nombre_usuario"];
+$email_usuario      = $_POST["email_usuario"];
+$password_usuario   = $_POST["password_usuario"];
+$rol_usuario        = 1;
+
+
+// Consulta para insertar
+$insertar   = "INSERT INTO usuario(nombre_usuario, email_usuario,
+password_usuario, rol_usuario) VALUES ('$nombre_usuario', '$email_usuario', '$password_usuario', '$rol_usuario')";
+
+$verificar_usuario = mysqli_query ($conexion, "SELECT * FROM usuario WHERE
+nombre_usuario = '$nombre_usuario' ");
+
+if(mysqli_num_rows($verificar_usuario) > 0){
+    echo 'El usuario ya esta registrado';
+    exit;
+}
+
+$verificar_correo = mysqli_query ($conexion, "SELECT * FROM usuario WHERE
+email_usuario = '$email_usuario' ");
+
+if(mysqli_num_rows($verificar_correo) > 0){
+    echo 'El usuario ya esta registrado';
+    exit;
+}
+
+//Ejecutar consulta
+
+$resultado = mysqli_query($conexion, $insertar);
+
+if (!$resultado){
+    echo 'Error al registrarse';
+} else {
+    echo 'Usuario registrado exitosamente';
+}
+
+//Cerrar conexión
+mysqli_close($conexion);
